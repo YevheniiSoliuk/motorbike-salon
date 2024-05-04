bdd07027116a247fad7eda849fd45dd389899721
patrick1671 <patryk15@icloud.com>
2024-05-04 14:19:12 +0200

    Add model viewer missing import, add sizing

--- frontend/src/Product/Model.tsx
+++ frontend/src/Product/Model.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import type { ModelViewerElement } from '@google/model-viewer/lib/model-viewer';
-
+import '@google/model-viewer/dist/model-viewer';
 type ModelProps = {
   modelUrl: string;
 };
@@ -10,27 +10,28 @@ const Model = ({ modelUrl }: ModelProps) => {
   console.log(modelRef.current);
 
   return (
-    // <div>
-    <model-viewer
-      touch-action='pan-y'
-      tone-mapping='aces'
-      id='first'
-      src={modelUrl}
-      seamless-poster
-      environment-image='neutral'
-      exposure={1.0}
-      interaction-prompt-threshold={0}
-      shadow-intensity={1}
-      ar
-      autoplay
-      ar-modes='webxr scene-viewer quick-look'
-      auto-rotate
-      camera-controls
-      camera-orbit='0deg 90deg 0deg 8.37364m'
-      alt='3D model'
-      ref={modelRef}
-    ></model-viewer>
-    // </div>
+    <div>
+      <model-viewer
+        style={{ width: '100%', height: '100%' }}
+        touch-action='pan-y'
+        tone-mapping='aces'
+        id='first'
+        src={'/assets/scene.glb'}
+        seamless-poster
+        environment-image='neutral'
+        exposure={1.0}
+        interaction-prompt-threshold={0}
+        shadow-intensity={1}
+        ar
+        autoplay
+        ar-modes='webxr scene-viewer quick-look'
+        auto-rotate
+        camera-controls
+        camera-orbit='0deg 90deg 0deg 8.37364m'
+        alt='3D model'
+        ref={modelRef}
+      ></model-viewer>
+    </div>
   );
 };
 
